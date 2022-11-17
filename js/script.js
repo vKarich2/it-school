92cb4d3247d1bfc5de6c583e32ae12a07e0b2d0a
vKarich2 <vladyslav.karapetov@nure.ua>
2022-11-17 17:56:20 +0100

    Slider

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 $(function () {
   $(".main-slider").slick({
+    slidesToShow: 3,
+    infinite: true,
+    dots: true,
+    arrows: false,
+    speed: 1500,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    centerMode: true,
+    centerPadding: "90px",
   });
 });
