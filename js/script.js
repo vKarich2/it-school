366d8fbae161ce9932416d216fe29e3d01507584
vKarich2 <vladyslav.karapetov@nure.ua>
2022-11-19 18:50:06 +0100

    Add before-img on cards

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 $(function () {
+  $("#menu").on("click", "a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr("href"),
+      top = $(id).offset().top;
+    $("body,html").animate({ scrollTop: top }, 1500);
+  });
+  
   $(".main-slider").slick({
     slidesToShow: 3,
     infinite: true,
